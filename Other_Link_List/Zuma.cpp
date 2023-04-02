9ce1f26d76c53eb58508e8f2bd175f482dfd136f
honjeng akasimina <1074368873@qq.com>
2023-04-03 00:03:14 +0800

    更新了重复删除、前驱后继和多项式运算

--- Other_Link_List/Zuma.cpp
+++ Other_Link_List/Zuma.cpp
@@ -58,3 +58,33 @@ void displayLink(Ball* header){
     cout << endl;
     
 }
+
+
+/*
+ int main(){
+     Ball* header = new Ball;
+     Ball* p = header;
+     char data;
+     
+     while( (data = getchar()) != '\n' ){
+         p->next = new Ball{data, p, NULL};
+         p = p->next;
+     }
+     *****************************
+     
+     int times, index;
+     cin >> times;
+     
+     while( times-- ){
+         cin >> index >> data;
+         p = InsertBall(index, data, header);
+         
+         while( (RecursiveCountN(p, p->data) + RecursiveCountP(p, p->data) - 1) > 2)
+             p = DeleteIt(p);
+             
+         displayLink(header);
+     }
+     
+     return 0;
+ }
+*/
