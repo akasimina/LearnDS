70091d2229b6be3e24214dfdd3c59e7b3330bcdc
honjeng akasimina <1074368873@qq.com>
2023-04-09 02:09:02 +0800

    the last three question, the last one has some problem that i haven' t sovled yet. Everything else is OK

---
+++ Stack Application/Char_Replacement.hpp
@@ -0,0 +1,13 @@
+//
+//  Char_Replacement.hpp
+//  DS
+//
+//  Created by 周子朋 on 2023/4/8.
+//
+
+#ifndef Char_Replacement_hpp
+#define Char_Replacement_hpp
+
+#include <stdio.h>
+
+#endif /* Char_Replacement_hpp */
---
+++ Stack Application/Maze_Solving.hpp
@@ -0,0 +1,13 @@
+//
+//  Maze_Solving.hpp
+//  DS
+//
+//  Created by 周子朋 on 2023/4/8.
+//
+
+#ifndef Maze_Solving_hpp
+#define Maze_Solving_hpp
+
+#include <stdio.h>
+
+#endif /* Maze_Solving_hpp */
