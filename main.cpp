11975d29314846ab3c2ada23569524a1b21e8d2f
honjeng akasimina <1074368873@qq.com>
2023-05-18 00:53:45 +0800

    new mainc.pp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,9 @@
 
-
 #include <iostream>
-#include "Other_Link_List/Zuma.hpp"
+#include "Exercises/BinaryTree/NumberOfLeafNode.cpp"
 using namespace std;
 
 int main(){
-    Ball* header = new Ball;
-    Ball* p = header;
-    char data;
-    
-    while( (data = getchar()) != '\n' ){
-        p->next = new Ball{data, p, NULL};
-        p = p->next;
-    }
-    /*************************/
-    
-    int times, index;
-    cin >> times;
-    
-    while( times-- ){
-        cin >> index >> data;
-        p = InsertBall(index, data, header);
-        
-        while( (RecursiveCountN(p, p->data) + RecursiveCountP(p, p->data) - 1) > 2)
-            p = DeleteIt(p);
-        
-        displayLink(header);
-    }
-    
+    Solution();
     return 0;
 }
